ba9d182005b1e90f942bd8f7f0c1e67ab2e0c687
Gautham Vasan <gauthamv.529@gmail.com>
2022-08-11 21:34:27 -0400

    Update defaults for learning stability

--- rl_suite/cc_expt.sh
+++ rl_suite/cc_expt.sh
@@ -11,5 +11,5 @@ module load cuda/11.2.2
 source /home/vasan/src/rtrl/bin/activate
 
 SECONDS=0
-python sac_experiment.py --description "reward_neg1_g995_t500" --init_steps 5000 --work_dir "/home/vasan/src/rl_suite/rl_suite/results/mj_reacher_tol9_baseline/sac" --N 151000 --gamma 0.995 --env "sparse_reacher" --tol 0.009 --timeout 500 --penalty 1 --actor_hidden_sizes "512 512" --critic_hidden_sizes "2048 2048" --l2_reg 0.0001 --seed $SLURM_ARRAY_TASK_ID
+python sac_experiment.py --description "reward_neg1_g995_t500" --init_steps 5000 --work_dir "/home/vasan/src/rl_suite/rl_suite/results/mj_reacher_tol9_baseline/sac" --N 201000 --gamma 0.995 --env "sparse_reacher" --tol 0.009 --timeout 500 --penalty 1 --actor_hidden_sizes "512 512" --critic_hidden_sizes "2048 2048" --l2_reg 0.0001 --seed $SLURM_ARRAY_TASK_ID
 echo "Baseline job $seed took $SECONDS"
