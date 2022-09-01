cad9d6d4e240365b5b6d1299046c5ad6017854a7
Gautham Vasan <gauthamv.529@gmail.com>
2022-09-01 15:55:18 -0400

    leaarn_to_reset script

--- rl_suite/cc_expt.sh
+++ rl_suite/cc_expt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=8  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=2-00:00
+#SBATCH --mem=16000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --time=1-00:00
 #SBATCH --output=%N-%j.out
 #SBATCH --account=rrg-ashique
 #SBATCH --array=1-30 
@@ -11,5 +11,5 @@ module load cuda/11.2.2
 source /home/vasan/src/rtrl/bin/activate
 
 SECONDS=0
-python sac_experiment.py --description "baseline_t100" --timeout 100 --algo "sac_rad" --work_dir "/home/vasan/scratch/reset_action/visual_dot_reacher/sac" --N 251000 --env "visual_dot_reacher" --seed $SLURM_ARRAY_TASK_ID --penalty -1
+python learn_to_reset.py --description "g99_rt09" --reset_thresh 0.9 --work_dir "/home/vasan/scratch/reset_action/dm_reacher_easy" --N 501000 --env "dm_reacher_easy" --seed $SLURM_ARRAY_TASK_ID --penalty -1
 echo "Baseline job $seed took $SECONDS"
