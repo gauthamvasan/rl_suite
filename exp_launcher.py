6ed9d0ad6f742bed60a5cbbf726a2f62c7ba93fa
Yan Wang <yan28@ualberta.ca>
2022-10-16 01:04:30 -0400

    fix exp settting

--- exp_launcher.py
+++ exp_launcher.py
@@ -23,19 +23,21 @@ non_visual_steps = {
 def generate_exps():
     exps = []
     
+    # exp settings
     algos = ["sac", "sac_rad"]
-    algos = ["sac_rad",]
+    algos = ["sac",]
     envs = ["ball_in_cup", "dm_reacher_hard", "dm_reacher_easy"]
     timeouts = [10, 25, 50, 100, 500, 1000, 5000]
-    timeouts = [50]
+    seeds = range(30)
     factor = 10
+
     for algo in algos:
         description = "2nd_paper_with_image" if algo == "sac_rad" else "2nd_paper_no_image"
         for timeout in timeouts:
             for env in envs:
                 N = visual_steps[env] if algo == "sac_rad" else non_visual_steps[env]
                 init_steps = N//factor
-                for seed in range(1):
+                for seed in seeds:
                     res_dir = 'results/'
                     exp_dir = env+('/visual' if algo == "sac_rad" else "/non_visual")+f"/timeout={timeout}/seed={seed}/"
                     output_folder = res_dir+exp_dir+'outputs/'
