58bb9b1bfb6dd4753001d42771732cc6bc913ffd
Gautham Vasan <gauthamv.529@gmail.com>
2023-02-01 14:13:09 -0500

    Hits vs timeout plot

--- rl_suite/plot/plot_hits_vs_timeout.py
+++ rl_suite/plot/plot_hits_vs_timeout.py
@@ -5,10 +5,34 @@ import pandas as pd
 
 from pathlib import Path
 
-for filename in Path(__file__).parent.glob('*random_stat.txt'):
-    env = re.match(r".*/(.+?)_.*", str(filename)).group(1)
+def find_files_and_plot():
+    """ Yan's original script preserved for posterity """
+    for filename in Path(__file__).parent.glob('*random_stat.txt'):
+        env = re.match(r".*/(.+?)_.*", str(filename)).group(1)
+        df = pd.DataFrame(columns=["timeout", "seed", "hits"])
+        with open(filename, 'r') as file:
+            for line in file.readlines():
+                match = re.match(r"timeout=(\d+), seed=(\d+): (\d+)", line)
+                timeout = match.group(1)
+                seed = match.group(2)
+                hits = match.group(3)
+                df = df.append({'timeout': int(timeout), 'seed':int(seed), 'hits': int(hits)}, ignore_index=True)
+
+        plt.title(env)
+        plt.xlabel('Timeout')
+        plt.ylabel('Hits')
+        ax = sns.barplot(data=df, x='timeout', y='hits')
+        ax.bar_label(ax.containers[0])
+        plt.savefig(f'{env}.png')
+        plt.close()
+
+def hits_vs_timeout():
+    env = "pendulum"
+    bp = "/home/vasan/src/rl_suite/rl_suite/plot"
+    fp = bp + f"/{env}_random_stat.txt"
+
     df = pd.DataFrame(columns=["timeout", "seed", "hits"])
-    with open(filename, 'r') as file:
+    with open(fp, 'r') as file:
         for line in file.readlines():
             match = re.match(r"timeout=(\d+), seed=(\d+): (\d+)", line)
             timeout = match.group(1)
@@ -23,3 +47,6 @@ for filename in Path(__file__).parent.glob('*random_stat.txt'):
     ax.bar_label(ax.containers[0])
     plt.savefig(f'{env}.png')
     plt.close()
+
+if __name__ == "__main__":
+    hits_vs_timeout()
