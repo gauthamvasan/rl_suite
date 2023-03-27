15b2c523a7790e0fda57b50ba14ecf491651cf54
Gautham Vasan <gauthamv.529@gmail.com>
2023-03-27 17:45:25 +0200

    Vision-based task support

--- rl_suite/envs/dot_tracker.py
+++ rl_suite/envs/dot_tracker.py
@@ -10,17 +10,16 @@ from rl_suite.envs import Observation
 from rl_suite.arp import ARProcess
 from gym.core import Env
 from gym.spaces.box import Box
+from collections import deque
 
 #instructions to windows to center the game window in the center of
 #the screen, which it might ignore
 os.environ["SDL_VIDEO_CENTERED"] = "1"
 
-# Initialize pygame
-pygame.init()
-
 
 class DotTracker(Env):
-    def __init__(self, dt=0.2, timeout=10000, pos_tol=0.05, penalty=-1, seed=42, use_image=False) -> None:
+    def __init__(self, dt=0.2, timeout=10000, pos_tol=0.05, penalty=-1, 
+                 seed=42, use_image=False, img_history=3) -> None:
         super().__init__()
         self.dt = dt
         self.pos_tol = pos_tol
@@ -29,11 +28,9 @@ class DotTracker(Env):
         self.use_image = use_image
         
         # Screen dimensions
-        self.width = self.height = 500
+        self.width = self.height = 1000
         self.screen_size = [self.width, self.height]
-        self.bg_color = pygame.Color("darkslategrey")
-        self.screen = pygame.display.set_mode(self.screen_size)
-        pygame.display.set_caption("Dot Tracker")
+        self.bg_color = pygame.Color("darkslategrey")        
 
         # Circle 
         self.radius = int(0.04 * self.width)
@@ -47,6 +44,14 @@ class DotTracker(Env):
         self.steps = 0
         self.seed(seed)
 
+        # Initialize pygame
+        pygame.init()
+        self.screen = pygame.display.set_mode(self.screen_size)
+        pygame.display.set_caption("Dot Tracker")
+
+        if use_image:
+            self._image_buffer = deque([], maxlen=img_history)            
+
     def seed(self, seed):
         np.random.seed(seed)
     
@@ -58,8 +63,16 @@ class DotTracker(Env):
         self.ar = ARProcess(3, 0.8, 2)
         self.prev_ar_pos = self.ar.step()[0]
         self.target_pos = np.random.uniform(-1, 1, 2)
-                
-        obs = np.concatenate((self.pos, self.vel))
+        
+        if self.use_image:
+            obs = Observation()
+            img = pygame.surfarray.array3d(self.screen)
+            for _ in range(self._image_buffer.maxlen):
+                self._image_buffer.append(img)
+            obs.images = np.concatenate(self._image_buffer, axis=0)
+            obs.proprioception = np.concatenate((self.pos, self.vel))
+        else:
+            obs = np.concatenate((self.pos, self.vel, self.target_pos))
         return obs
 
     def step(self, action):
@@ -89,8 +102,29 @@ class DotTracker(Env):
 
         self.vel += action * self.dt
 
+        ###
+        self.screen.fill(self.bg_color)
+    
+        # Keep circle within boundary visually
+        pos = np.clip(self.pos, -0.92, 0.92)
+        pixel_pos = self.pos_to_pixel(pos)
+        
+        # Keep target within boundary visually
+        target_pos = self.pos_to_pixel(self.target_pos)
+        target_pos = np.clip(target_pos, 0, self.width - 0.08*self.width)
+        
+        pygame.draw.rect(self.screen, self.target_color, target_pos.tolist() + [self.target_width, self.target_width])
+        pygame.draw.circle(self.screen, self.circle_color, pixel_pos, self.radius)
+        ###
+
         # Observation
-        next_obs = np.concatenate((self.pos, self.vel))
+        if self.use_image:
+            next_obs = Observation()
+            next_obs.proprioception = np.concatenate((self.pos, self.vel))            
+            self._image_buffer.append(pygame.surfarray.array3d(self.screen))
+            next_obs.images = np.concatenate(self._image_buffer, axis=0)
+        else:
+            next_obs = np.concatenate((self.pos, self.vel, self.target_pos))
 
         # Reward
         reward = self.penalty
@@ -108,18 +142,6 @@ class DotTracker(Env):
         return pixel_pos
 
     def render(self):
-        self.screen.fill(self.bg_color)
-        
-        # Keep circle within boundary visually
-        pos = np.clip(self.pos, -0.92, 0.92)
-        pixel_pos = self.pos_to_pixel(pos)
-        
-        # Keep target within boundary visually
-        target_pos = self.pos_to_pixel(self.target_pos)
-        target_pos = np.clip(target_pos, 0, self.width - 0.08*self.width)
-
-        pygame.draw.circle(self.screen, self.circle_color, pixel_pos, self.radius)
-        pygame.draw.rect(self.screen, self.target_color, target_pos.tolist() + [self.target_width, self.target_width])
         pygame.display.update()
 
     @property
@@ -141,7 +163,7 @@ if __name__ == "__main__":
     n_episodes = 100
     timeout = 20000
     seed = 42
-    env = DotTracker(dt=0.2, timeout=timeout) 
+    env = DotTracker(dt=0.2, timeout=timeout, pos_tol=0.1, use_image=False) 
 
     for i_episode in range(n_episodes):
         obs = env.reset() 
@@ -161,6 +183,5 @@ if __name__ == "__main__":
             ep_len += 1
         
         print(f"Episode {i_episode+1} took {ep_len} steps and ended with return {ret}. Total steps: {steps}")
-               
 
-    # env.close()
+    env.close()
