e8e5941641a3c5e68a1ee95bfca41c0e427a1628
Gautham Vasan <gauthamv.529@gmail.com>
2023-10-09 16:23:44 -0400

    Remove title

--- rl_suite/plot/plot.py
+++ rl_suite/plot/plot.py
@@ -166,9 +166,9 @@ def point_maze_steps_to_goal_plot():
         "point_maze_min_time_dense": "royalblue",
     }
 
-    # basepath = "/home/vasan/scratch/tro_paper/min_time_K500"
-    basepath = "/home/vasan/scratch/tro_paper"
-    key = "U"
+    basepath = "/home/vasan/scratch/tro_paper/min_time_K500"
+    # basepath = "/home/vasan/scratch/tro_paper"
+    key = "min_time"
 
     for env, color in color_palette.items():
         if key not in env:
@@ -199,7 +199,7 @@ def point_maze_steps_to_goal_plot():
     print(color_palette)
     # sns.lineplot(x="step", y='avg_ret', data=df[df['env'].str.contains('point_maze_T_dense')], hue='env', palette=color_palette)
     sns.lineplot(x="step", y='steps_to_goal', data=df[df['env'].str.contains(key)], hue='env', palette=color_palette)
-    # set_labels("Point Maze", labelpad=25)
+    set_labels("", labelpad=25)
 
     # plt.legend(handles=legend_elements, loc='lower right')
     plt.legend(handles=legend_elements, loc='best')
